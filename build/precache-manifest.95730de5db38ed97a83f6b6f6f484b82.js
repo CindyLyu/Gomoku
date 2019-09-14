cc7e8a6ccee789092c66696aa8d555c2f18c5728
CindyLyu <xd753159@gmail.com>
2019-09-14 20:47:16 +0800

    add Gomoku completed

---
+++ build/precache-manifest.95730de5db38ed97a83f6b6f6f484b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0aa3b8850be3a2087be9f15f428fd9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2601d53c38a0597ca045",
+    "url": "/static/css/main.9624ae7b.chunk.css"
+  },
+  {
+    "revision": "f478c3e6a5a59dee73f6",
+    "url": "/static/js/2.81f98875.chunk.js"
+  },
+  {
+    "revision": "2601d53c38a0597ca045",
+    "url": "/static/js/main.a041ee13.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  }
+]);
